c9429b446bb0786f7f39c8878cfa5d850043f565
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-07-02 22:30:36 +0530

    hangman added

--- src/pages/PlayGame.jsx
+++ src/pages/PlayGame.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import MaskedText from '../components/MaskedText/MaskedText';
+import LetterButtons from '../components/LetterButtons/LetterButtons';
+import HangMan from '../components/HangMan/HangMan';
 
 function PlayGame() {
 
   const {state} = useLocation();
-  console.log(state);
+  // console.log(state);
+
+  const [guessedLetters, setGussedLetters] = useState([]);
+  const [step, setStep] = useState(0);
+
+  function handleLetterClick(letter){
+    if(state.wordSelected.toUpperCase().includes(letter)){
+      console.log("correct");
+    }else{
+      setStep(step+1);
+      console.log("Wrong");
+    }
+    setGussedLetters([...guessedLetters, letter]);
+
+  }
 
   return (
     <>
         <div>PlayGame {state.wordSelected}</div>
+
+        <MaskedText text={state.wordSelected} guessedLetters={guessedLetters}/>
+
+        <div>
+          <LetterButtons text = {state.wordSelected} guessedLetters={guessedLetters} onLetterClick={handleLetterClick} />
+        </div>
+        <div>
+          <HangMan step = {step} />
+        </div>
         <Link to = "/start" className='text-blue-400'>Start game Link</Link>
+
+
 
     </>
     
